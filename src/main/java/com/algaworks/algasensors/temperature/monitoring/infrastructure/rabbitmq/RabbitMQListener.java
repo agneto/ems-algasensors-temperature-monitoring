d026feef38f12cf45d9e9c502e1711ebec4e1c93
abilioneto <abilio@bsd.com.br>
2025-06-07 17:56:00 -0300

    rabbitmq implements

---
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
@@ -0,0 +1,29 @@
+package com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq;
+
+import com.algaworks.algasensors.temperature.monitoring.api.model.TemperatureLogData;
+import com.algaworks.algasensors.temperature.monitoring.domain.service.TemperatureMonitoringService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+import static com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq.RabbitMQConfig.QUEUE;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RabbitMQListener {
+
+    private final TemperatureMonitoringService temperatureMonitoringService;
+
+    @RabbitListener(queues = QUEUE, concurrency = "2-3")
+    public void handle(@Payload final TemperatureLogData temperatureLogData,
+                       @Headers final Map<String, Object> headers) {
+        log.info("Headers ::::: {}", headers);
+        this.temperatureMonitoringService.processTemperatureReading(temperatureLogData);
+    }
+}
